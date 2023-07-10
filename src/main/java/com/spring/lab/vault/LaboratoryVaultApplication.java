25ede02f2fcbe2d29c70774af1127ede2cae4f32
juan.cardona <felipe7u7432@gmail.com>
2023-07-09 19:30:20 -0500

    crea proyecto base

---
+++ src/main/java/com/spring/lab/vault/LaboratoryVaultApplication.java
@@ -0,0 +1,13 @@
+package com.spring.lab.vault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratoryVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboratoryVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/lab/vault/LaboratoryVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.lab.vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratoryVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
