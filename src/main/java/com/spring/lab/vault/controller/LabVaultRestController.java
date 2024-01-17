ef4c4d0ff6a3027e9300d63983b2b21dac231519
juan.cardona <felipe7u7432@gmail.com>
2024-01-16 23:21:42 -0500

    #master: Ensayo commit 3. Prueba de commit.

---
+++ src/main/java/com/spring/lab/vault/controller/LabVaultRestController.java
@@ -0,0 +1,8 @@
+package com.spring.lab.vault.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RequestMapping("api/v1/laboratoryvault/labvaultcontroller")
+@RestController
+public class LabVaultRestController {
+}
